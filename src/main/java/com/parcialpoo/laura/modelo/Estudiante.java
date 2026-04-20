45a58b03cb8a761247f8566a6c14c574b444a373
lauto <lauto@LAURA>
2026-04-20 00:18:12 -0500

    Implementación clase Estudiante con herencia

--- src/main/java/com/parcialpoo/laura/modelo/Estudiante.java
+++ src/main/java/com/parcialpoo/laura/modelo/Estudiante.java
@@ -1,7 +1,9 @@
 package com.parcialpoo.laura.modelo;
-
+// Clase Estudiante que hereda de Persona
 public class Estudiante extends Persona {
-    private String codigo, programa;
+
+    private String codigo;
+    private String programa;
 
     public Estudiante(String nombre, String direccion, String telefono, String fechaNacimiento,
                       String codigo, String programa) {
@@ -10,5 +12,22 @@ public class Estudiante extends Persona {
         this.programa = programa;
     }
 
-    public String getCodigo() { return codigo; }
+  @Override
+public String mostrarDatos() {
+    return "Nombre: " + getNombre() +
+           "\nDirección: " + getDireccion() +
+           "\nTeléfono: " + getTelefono() +
+           "\nFecha Nacimiento: " + getFechaNacimiento() +
+           "\nCódigo: " + codigo +
+           "\nPrograma: " + programa +
+           "\n----------------------";
+} 
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    private String getEdad() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
