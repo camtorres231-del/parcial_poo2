ae45aaafd500bbb564f0412ea491141af1c46558
lauto <lauto@LAURA>
2026-04-20 00:27:57 -0500

    Implementación clase principal Main

--- src/main/java/com/parcialpoo/laura/principal/Main.java
+++ src/main/java/com/parcialpoo/laura/principal/Main.java
@@ -1,22 +1,14 @@
 package com.parcialpoo.laura.principal;
+// Clase principal de ejecución del sistema
 
-import com.parcialpoo.laura.modelo.*;
-import com.parcialpoo.laura.controlador.*;
+import com.parcialpoo.laura.controlador.Colegio;
 import com.parcialpoo.laura.vista.VentanaPrincipal;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Colegio c = new Colegio();
-
-        Profesor p1 = new Profesor("Juan","Cali","123","2000",
-                "111","Matematicas",10000,100);
-
-        Estudiante e1 = new Estudiante("Ana","Cali","321","2005","01","Ingenieria");
-
-        c.agregarProfesor(p1);
-        c.agregarEstudiante(e1);
-
         VentanaPrincipal v = new VentanaPrincipal(c);
         v.setVisible(true);
     }
